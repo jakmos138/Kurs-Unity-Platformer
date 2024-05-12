17be83f3559250c59a457887668601b88939a823
jakmos138 <jakub.moszynski138@gmail.com>
2024-05-12 12:56:02 +0200

    Aiming Functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,16 +11,52 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public LayerMask groundLayer;
     public GameObject projectile;
+    public Transform gunArm;
+
+    private Camera worldCamera;
     private Vector2 movement;
+
+    private void Start()
+    {
+        worldCamera = FindObjectOfType<Camera>();
+    }
 
     void Update()
     {
         grounded = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.1f, groundLayer);
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), grounded ? (Input.GetKey(KeyCode.W) ? jumpPower : rb.velocity.y) : rb.velocity.y);
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), grounded ? (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space) ? jumpPower : rb.velocity.y) : rb.velocity.y);
+        Aim();
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(movement.x * speed * Time.fixedDeltaTime, movement.y);
+    }
+
+    private Vector3 GetMousePos()
+    {
+        Vector3 mousePos = worldCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        return mousePos;
+    }
+
+    private void Aim()
+    {
+        Vector3 mousePos = GetMousePos();
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        gunArm.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 localScale = Vector3.one;
+        if (Mathf.Abs(angle) > 90)
+        {
+            localScale.y = -1f;
+        }
+        else
+        {
+            localScale.y = +1f;
+        }
+
+        gunArm.localScale = localScale;
     }
 }
